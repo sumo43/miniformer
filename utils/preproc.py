a91e2fae47f7832e367529c689ba24e47a437239
yatsenkoa <yatsenkoa2020@gmail.com>
2022-06-09 00:08:50 -0400

    huggingface kinda nice

--- utils/preproc.py
+++ utils/preproc.py
@@ -6,31 +6,16 @@ from collections import Counter
 from torchtext.utils import download_from_url, extract_archive
 from utils.general import to_words
 from utils.data import load_data
-from transformers import Tokenizer
+from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.processors import TemplateProcessing
 from torch.utils.data import Dataset, DataLoader
-#from transformers import PreTrainedTokenizerFast
+from transformers import PreTrainedTokenizerFast
 import io
 import os
 
-"""
-def pad_sentences(mp, ds, tokenizer, voc):
-
-    out_ds = []
-
-    for i in ds:
-        e = tokenizer(i)
-        e = ['<bos>', *e, '<eos>']
-        num_pad_tokens = mp.max_seq_length - len(e)
-        [e.append('<pad>') for i in range(num_pad_tokens)]
-
-        out_ds.append(torch.tensor([es_vocab[a] for a in e]))
-    
-    return out_ds
-"""
 
 EN_DEV_LOC = os.path.join('data', 'opus.en-es-dev.en')
 ES_DEV_LOC = os.path.join('data', 'opus.en-es-dev.es')
@@ -52,7 +37,7 @@ class TransformerDataset:
         self.dev = None
 
         if os.path.exists(TOK_FILE_LOC):
-            tokenizer = Tokenizer.from_file(TOK_FILE_LOC)
+            tokenizer = PreTrainedTokenizerFast(tokenizer_file=TOK_FILE_LOC)
             
         else:
             tokenizer = Tokenizer(BPE(unk_token="[UNK]", bos_token='[BOS]', eos_token='[EOS]', pad_token='[PAD]'))
@@ -79,114 +64,31 @@ class TransformerDataset:
             )
 
             tokenizer.train(files, trainer)
-            tokenizer.enable_padding(pad_id=3, pad_token='[PAD]')
             tokenizer.save(TOK_FILE_LOC)
             
-        
         raw_en_ds = load_data(EN_DEV_LOC)
         raw_es_ds = load_data(ES_DEV_LOC)
 
-        print(tokenizer(raw_en_ds))
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
-        train_en_ds = torch.utils.data.DataLoader(raw_en_ds, batch_size=32, shuffle=True, collate_fn = lambda x: tokenizer.encode_batch(x))
-        train_es_ds = torch.utils.data.DataLoader(raw_en_ds, batch_size=32, shuffle=True, collate_fn = lambda x: tokenizer.encode_batch(x)) 
+        train_en_ds = torch.utils.data.DataLoader(raw_en_ds, batch_size=32, shuffle=True, collate_fn = lambda x: tokenizer(x, padding='longest'))
+        train_es_ds = torch.utils.data.DataLoader(raw_en_ds, batch_size=32, shuffle=True, collate_fn = lambda x: tokenizer(x, padding='longest')) 
 
-        for item in train_en_ds:
+        self.train_en_ds = train_en_ds
+        self.train_es_ds = train_es_ds
 
-            item = [torch.tensor(a.ids) for a in item]
+        """
+        self.train_en_ds_iter = iter(self.train_en_ds)
+        self.train_es_ds_iter = iter(self.train_es_ds)
+        """
 
-            print(item[0].shape)
-            print(item[-1].shape)
+        self.tokenizer = tokenizer
 
-            return
+    def get_en(self):
+        return self.train_en_ds
 
-        print('tokenizer loaded')
-        print(tokenizer.encode('Name jeff'))
+    def get_es(self):
+        return self.train_es_ds
 
-
-
-def get_vocabs(mp, _input, _output, test=True):
-    if test:
-        _input = _input[-10000:]
-        _output = _output[-10000:]
-
-    en_tokenizer = get_tokenizer('moses', language='en')    
-    train_iter = iter(_input)
-
-    en_vocab = build_vocab_from_iterator(map(en_tokenizer, train_iter), specials=['<unk>', '<pad>', '<bos>', '<eos>'])
-
-    es_tokenizer = get_tokenizer('moses', language='es')
-    val_iter = iter(_output)
-    es_vocab = build_vocab_from_iterator(map(es_tokenizer, val_iter), specials=['<unk>', '<pad>', '<bos>', '<eos>'])
-
-    return en_vocab, es_vocab
-
-
-
-def preprocess_data(mp, train, val, test=False):
-
-    if test:
-        train = train[-10000:]
-        val = val[-10000:]
-
-    en_tokenizer = get_tokenizer('moses', language='en')    
-    train_iter = iter(train)
-    batch_size = mp.batch_size
-    device = mp.device
-
-    en_vocab = build_vocab_from_iterator(map(en_tokenizer, train_iter), specials=['<unk>', '<pad>', '<bos>', '<eos>'])
-
-    # these will be our inputs. list of numbers corr. to list of numbers in spanish vocab
-
-    in_ds = []
-
-    curr_batch = []
-
-    for it, i in enumerate(train):
-
-        e = en_tokenizer(i)
-        e = ['<bos>', *e, '<eos>']
-        num_pad_tokens = mp.max_seq_length - len(e)
-        [e.append('<pad>') for i in range(num_pad_tokens)] 
-        curr_batch.append(torch.tensor([en_vocab[a] for a in e]).unsqueeze(0).to(device))
-
-        if it != 0 and it % batch_size == 0:
-            curr_batch = torch.cat(curr_batch)
-            in_ds.append(curr_batch)
-            curr_batch = []
-
-    curr_batch = [] 
-
-    es_tokenizer = get_tokenizer('moses', language='es')
-    val_iter = iter(val)
-    es_vocab = build_vocab_from_iterator(map(es_tokenizer, val_iter), specials=['<unk>', '<pad>', '<bos>', '<eos>'])
-
-    out_ds = []
-    
-    for it, i in enumerate(val):
-        e = es_tokenizer(i)
-        e = ['<bos>', *e, '<eos>']
-        num_pad_tokens = mp.max_seq_length - len(e)
-        [e.append('<pad>') for i in range(num_pad_tokens)]
-        curr_batch.append(torch.tensor([es_vocab[a] for a in e]).unsqueeze(0).to(device))
-
-        if it != 0 and it % batch_size == 0:
-            curr_batch = torch.cat(curr_batch)
-            out_ds.append(curr_batch)
-            curr_batch = []
-
-    mp.set_en_vocab_size(len(en_vocab))
-    mp.set_es_vocab_size(len(es_vocab))
-
-    return in_ds, out_ds, en_vocab, es_vocab
-
-def postprocess_data(mp, data):
-    data = data.argmax(dim=1)
-    return data
-
-def to_string(data, vocab):
-    itos = vocab.get_itos()
-    st = ''
-    for i in data:
-        st += itos[i] + ' ' 
-    return st
+    def get_tokenizer(self):
+        return self.tokenzizer
