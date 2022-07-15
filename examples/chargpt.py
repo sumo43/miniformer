70d8da76c52bf06e486aeaa3d96c812fcd9079e4
yatsenkoa <yatsenkoa2020@gmail.com>
2022-07-15 19:23:03 -0400

    ok so its not the attention...

--- examples/chargpt.py
+++ examples/chargpt.py
@@ -23,11 +23,10 @@ chargpt_config = {
     'n_decoders' : 4, # number of decoder layers
     'max_seq_length' : 128, # max m
     # hyperparameters for training
-    'lr' : 4e-5, # use lr scheduler later
-    'vocab_size': 30,
+    'lr' : 4e-4, # use lr scheduler later
+    'vocab_size': 39,
     'epochs': 10,
     'batch_size': 32
-
 }
 
 class CharDataset:
@@ -37,8 +36,8 @@ class CharDataset:
         self.data = self.data.lower() # lowecase only
         # count the occurrences of each char and sort them
         chars = sorted(Counter(list(self.data)).items(), key=lambda a: a[1], reverse=True)
-        self.ctoi = {c[0]: i+1 for i,c in enumerate(chars)}
-        self.ctoi['<S>'] = 0 # make space for SPECIAL_TOKEN at 0
+        self.ctoi = {c[0]: i for i,c in enumerate(chars)}
+        #self.ctoi['<S>'] = 0 # make space for SPECIAL_TOKEN at 0
         self.config.vocab_size = len(self.ctoi)
         self.itoc = {v:k for k, v in self.ctoi.items()}
 
@@ -49,7 +48,9 @@ class CharDataset:
         return self.itoc[x]
     
     def __getitem__(self, idx):
-        data = torch.Tensor(list(map(self.get_ctoi, self.data[idx:idx+config.max_seq_length+1]))).type(torch.LongTensor)
+        data = torch.Tensor(list(map(self.get_ctoi, self.data[idx:idx+config.max_seq_length+1])))\
+        .type(torch.LongTensor)\
+        .to(self.config.device)
         x = data[:-1]
         y = data[1:]
         return x, y
@@ -70,5 +71,3 @@ tok_text = list(map(dataset.get_ctoi, ex_text))
 out_tok = model(torch.tensor(tok_text).type(torch.LongTensor).to(config.device))
 
 print(str(map(dataset.get_itoc, out_tok)))
-
-
