b4a1c2295fd366e51ea867a7b23d9b71206a8d39
yatsenkoa <yatsenkoa2020@gmail.com>
2022-07-20 10:41:19 -0400

    return of the einops

--- miniformer/model.py
+++ miniformer/model.py
@@ -8,7 +8,6 @@ import time
 from einops.einops import rearrange
 import torch.nn.functional as F
 import einops
-import torchvision
 from einops.layers.torch import Rearrange
 
 """
@@ -56,55 +55,60 @@ class Config():
     def __init__(self, **kwargs): 
         self.__dict__.update(kwargs)
 
-class MHA(nn.Module):
-
-    """
-    A vanilla multi-head masked self-attention layer with a projection at the end.
-    It is possible to use torch.nn.MultiheadAttention here but I am including an
-    explicit implementation here to show that there is nothing too scary here.
-    Protip: Don't use einops... hard to debug
+class MHA(torch.nn.Module):
+    
+    """ 
+    Multi Head Attention for the Encoder. 
+    Params:
+    m: length of the input to the Encoder.
+    k: key dimension
+    v: value dimension
+    d: embedding dimension (d_model)
+    h: number of heads
+    b: batch dimension
+    Args:
+    x: a vector with shape [b d]. This becomes Q, K, V
+    P_q: a tensor with shape [h, d, k]. model weights.
+    P_k: a tensor with shape [h, d, k]. model weights.
+    P_v: a tensor with shape [h, d, v]. model weights.
+    P_o: a tensor with shape [h, d, v]. model weights.
+    Returns : 
+    y: a vector with shape [b, d]
     """
 
     def __init__(self, config, mask=False):
         super().__init__()
-        # key, query, value projections for all heads, but in a batch
-        self.l1 = nn.Linear(config.d, 3 * config.d)
-        self.l2 = nn.Linear(config.d, config.d)
-        # regularization
-        self.attn_dropout = nn.Dropout(0.1)
-        self.resid_dropout = nn.Dropout(0.1)
-        # causal mask to ensure that attention is only applied to the left in the input sequence
-        self.register_buffer("bias", torch.tril(torch.ones(config.max_seq_length, config.max_seq_length))
-                                     .view(1, 1, config.max_seq_length, config.max_seq_length))
-        self.d = config.d
-        self.h = config.h
+        self.P_q = torch.nn.Parameter(torch.nn.init.kaiming_uniform_(torch.zeros(config.h, config.d, config.k)), requires_grad=True)
+        self.P_k = torch.nn.Parameter(torch.nn.init.kaiming_uniform_(torch.zeros(config.h, config.d, config.v)), requires_grad=True)
+        self.P_v = torch.nn.Parameter(torch.nn.init.kaiming_uniform_(torch.zeros(config.h, config.d, config.v)), requires_grad=True)
+        self.P_o = torch.nn.Parameter(torch.nn.init.kaiming_uniform_(torch.zeros(config.h, config.d, config.v)), requires_grad=True)
+        self.register_buffer('triu_mask', torch.tril(torch.ones((config.d, config.d))).view(1, 1, config.d, config.d))
+        self.softmax = torch.nn.Softmax(dim=-1)
+        self.attn_dropout = torch.nn.Dropout(config.dropout)
+        self.resid_dropout = torch.nn.Dropout(config.dropout)
         self.config = config
-        self.mask = mask
+        self.rearr = Rearrange('b h m v -> b m (h v)')
 
     def forward(self, x):
-        config = self.config
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k ,v  = self.l1(x).split(self.d, dim=2)
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        q = q.view(B, T, config.h, config.k).transpose(2, 1)
-        k = k.view(B, T, config.h, config.k).transpose(2, 1)
-        v = v.view(B, T, config.h, config.v).transpose(2, 1)
-        logits = torch.einsum('bhmk, bhnk -> bhmn', q, k)
-        logits = logits / torch.sqrt(torch.tensor(self.config.k))
-        if self.mask:
-            att = logits.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        else:
-            att = logits
-        att = F.softmax(att, dim=-1)
-        att = self.attn_dropout(att)
-        y = torch.einsum('bhmm, bhmv -> bhmv', att, v)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-        # output projection
-        y = self.resid_dropout(self.l2(y))
-        return y
+        # linear layers before attention
+        Q = torch.einsum('bmd, hdk -> bhmk', x, self.P_q)
+        K = torch.einsum('bmd, hdk -> bhmk', x, self.P_k)
+        V = torch.einsum('bmd, hdv -> bhmv', x, self.P_v)
+        # attention
+        logits = torch.einsum('bhmk, bhnk -> bhmn', Q, K)
+        logits = logits / 1.0 * math.sqrt(self.config.k)
+        logits = self.mask(logits)
+        key_weights = self.softmax(logits)
+        key_weights = self.attn_dropout(key_weights)
+        o = torch.einsum('bhmm, bhmv -> bhmv', key_weights, V)
+        # linear layer after attention
+        y = torch.einsum('bhmv, hdv -> bmd', o, self.P_o)
+        return self.resid_dropout(y)
 
+    def mask(self, x):
+        seq_length = x.shape[-1]
+        return x.masked_fill(self.triu_mask[:, :, :seq_length, :seq_length] == 0, float('-inf'))
+        
 class Block(Module):
     """
     A transformer block. Can act as both an encoder (ViT) or standalone decoder (GPT).
@@ -167,44 +171,40 @@ class ViT(Module):
         self.out_size = config.vocab_size
         patch_size = (4, 4)
         flat_patch_size = 16
-        self.learnable_patch = torch.nn.Parameter(torch.randn(4 * 4,)).view(1, 1, 16)
+        self.learnable_patch = torch.nn.Parameter(torch.randn(4 * 4,))
         self.pos_embedding = torch.nn.Embedding(config.max_seq_length, config.d)
         self.patch_embedding = torch.nn.Linear(flat_patch_size, config.d)
-        
         self.encoder = nn.Sequential(
             *[Block(config, mask=False) for i in range(config.n_decoders)])
-
         self.head = nn.Sequential(
             torch.nn.Linear(config.d, config.d * 4),
             torch.nn.Linear(config.d * 4, config.d),
-            torch.nn.Linear(config.d, 10)
+            torch.nn.Linear(config.d, self.out_size)
         )  
         self.dropout = torch.nn.Dropout(config.dropout)
         self.ln = torch.nn.LayerNorm(config.d)
-        self.dumb = torch.nn.Linear(28 * 28, 128)
-        self.dumb2 = torch.nn.Linear(128, 10)
-
+         
     def forward(self, _input):
-        batch_size, channels, width, height = _input.shape
-        pos = torch.arange(0, 49, dtype=torch.long, device=self.config.device).unsqueeze(0) # shape (1, t)
+        pos = torch.arange(0, _input.shape[-1], dtype=torch.long, device=self.config.device).unsqueeze(0) # shape (1, t)
         # encode the input as a bunch of flattened patches (last dim)
         # then append the learnable patch at zero-index. now we have a nice even seq length
-        #_input = torch.cat([self.learnable_patch.expand(_input.shape[0], -1, 16), self.to_patches(_input)], dim=1)  
-        _input = self.to_patches(_input)   
+        _input = torch.cat([self.learnable_patch, self.to_patches(_input)], dim=-1)         
         input_embeddings = self.patch_embedding(_input) # -> (b, l_seq, d_model)
+        pos_emb = self.pos_encoding(self.pos)
         pos_embeddings = self.pos_embedding(pos)
         _input = self.dropout(input_embeddings + pos_embeddings)
         encoder_output = self.encoder(_input)
         encoder_output = self.ln(encoder_output)
-        encoder_output = torch.mean(encoder_output, 1)
         _output = self.head(encoder_output)
         return _output
-
-    def to_patches(self, img):
+    
+    @staticmethod
+    def to_patches(arr):
         # b, 28, 28,
-        batch_size, _, _, _ = img.shape
-        img = torch.nn.Unfold((4, 4), stride=4)(img).view(batch_size, 4, 4, 49)
+        b_dim = config.batch_size
+        img = torchvision.transforms.ToTensor()(img)
+        img = torch.nn.Unfold((4, 4), stride=4)(img).view(config.batch_size, 4, 4, 49)
         img = img.transpose(3, 1)
         img = img.transpose(2, 3)
-        img = img.view(batch_size, 49, -1)
+        img = img.view(config.batch_size, 49, -1)
         return img
